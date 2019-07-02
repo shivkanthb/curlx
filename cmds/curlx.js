00ec3a17987d09452bb93e44a8f844e0869d6577
Adam Mazzarella <3219297+apm963@users.noreply.github.com>
2019-07-02 13:10:33 -0400

    Handle curl exception scenario
    The `execCurl` function has the potential to return null which was not properly being handled downstream.

--- cmds/curlx.js
+++ cmds/curlx.js
@@ -5,6 +5,10 @@ const { execCurl } = require('../utils/execute-curl.js');
 
 module.exports = async (args, exec_str, db) => {
   let cx_response = await execCurl(exec_str);
+  if (!cx_response) {
+    outputResponse(`Error while executing ${exec_str}`);
+    return;
+  }
   outputResponseHeaders(cx_response.responseHeaders);
   outputResponse(cx_response.responseBody);
   if (cx_response.logData) {
